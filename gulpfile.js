9ba9df30b058f3d99472e29d5735b150e789d036
颜回 <yanhui@51huxin.com>
2016-08-11 07:11:46 +0800

    安装sass编译和终端输出

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
 var gulp = require('gulp');
-var minify = require('minify');
+// var minify = require('minify');
 
+//jade和scss编译
 var jade = require('gulp-jade');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // 将你的默认的任务代码放在这
-  return gulp.src('./src/page/*.jade')
-    .pipe(jade({
-      pretty: true
-    })).pipe(gulp.dest('./dest'))
+//终端输出标记
+var chalk = require('chalk');
+
+gulp.task('default', function(){
+	// 将你的默认的任务代码放在这
+	console.log('开始默认初始化');
+	chalk.yellow('start server');
 });
+
+
+gulp.task('compile', function() {
+  // jade和scss编译
+	gulp.src('./src/page/*.jade')
+	.pipe(jade({
+	  pretty: true
+	})).pipe(gulp.dest('./dest'))
+
+	gulp.src('./src/css/*.scss',{base : 'src'})
+	.pipe(sass()).pipe(gulp.dest('./dest'))
+});
+
+
+gulp.task('build',['default','compile'],function(){
+	console.log('结束任务');
+})
