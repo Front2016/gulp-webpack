678ba17c3e11374cbca73ebe60e53000b7c9e09e
颜回 <yanhui@51huxin.com>
2016-08-11 17:26:44 +0800

    文件权限，数据文件，文件大小检测，文件过滤筛选，推送到gitpages，压缩html，gulp条件判断，html模版渲染，工具类（日志,修改文件名etc）

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,16 @@ var gulp = require('gulp');
 //文件监控
 var watch = require('gulp-watch');
 
+/******************************* 编译系列 *****************************************/
+
 //jade和scss编译
 var jade = require('gulp-jade');
 var sass = require('gulp-sass');
 
 //该插件能够实现 只编译或打包改变过文件 ，大大加快了gulp task的执行速度。比如只操作发生过改变的文件。
 var changed = require('gulp-changed');
+
+/******************************* 压缩系列 *****************************************/
 
 //压缩css
 var minifycss = require('gulp-minify-css');
@@ -33,9 +37,11 @@ var pngquant = require('imagemin-pngquant');
 //使用”gulp-cache”只压缩修改的图片，没有修改的图片直接从缓存文件读取
 //（C:\Users\Administrator\AppData\Local\Temp\gulp-cache）
 var cache = require('gulp-cache');
-
-//重命名文件
-var rename = require('gulp-rename');
+//压缩html
+var htmlmin = require('gulp-htmlmin');
+//gulp.src('src/*.html')
+// .pipe(htmlmin({collapseWhitespace: true}))
+// .pipe(gulp.dest('dist'))
 
 //添加浏览器前缀,来自动兼容不同的浏览器，不如css3里边不同浏览器的差异。ex:transition
 //-webkit-transition:
@@ -55,13 +61,77 @@ var debug = require('gulp-debug');
 var browserSync = require('browser-sync');
 var bs = browserSync.create('My server');
 
+//数据支持
+//通过文件的方式，给jade文件提供传递数据，便于接口调试
+var data = require('gulp-data');
+// data(function(file, callback) {
+//   return callback(undefined, { 'foo': 'bar' });
+// })
+
+//数据渲染
+var nunjucksRender = require('gulp-nunjucks-render');
+// gulp.src('src/templates/*.html')
+// .pipe(data(getDataForFile))
+// .pipe(nunjucksRender({
+//   path: 'src/templates'
+// }))
+// 用来渲染html结构的
+//<link rel="stylesheet" href="{{ css_path }}test.css" />
+
+//文件过滤
+//跟gulp.src类似，但是支持字符流的暂存和回退
+var filter = require('gulp-filter');
+
+//gulp条件判断
+var gulpif = require('gulp-if');
+// var condition = true;
+// gulp.src('./src/*.js')
+// .pipe(gulpif(condition, uglify()))
+// .pipe(gulp.dest('./dist/'));
+
+//gulp任务执行顺序
+var sequence = require('gulp-sequence');
+// 1. run 'a', 'b' in parallel; 
+// 2. run 'c' after 'a' and 'b'; 
+// 3. run 'd', 'e' in parallel after 'c'; 
+// 3. run 'f' after 'd' and 'e'. 
+// gulp.task('sequence-1', gulpSequence(['a', 'b'], 'c', ['d', 'e'], 'f'));
+
+/******************************* 文件操作系列 *****************************************/
 
 //文件操作
 //删除文件
 var del = require('del');
+//重命名文件
+var rename = require('gulp-rename');
+//统计输出文件大小
+var size = require('gulp-filesize');
+// gulp.src('./css/*.css')
+// .pipe(gulp.dest('./dist/')
+// .pipe(size()) // [gulp] Size example.css: 265.32 kB 
+
+//日志，文件名修改等
+var gutil = require('gulp-util');
+// gutil.log('stuff happened', 'Really it did', gutil.colors.magenta('123'));
+// gutil.beep();
+// gutil.replaceExtension('file.coffee', '.js'); // file.js 
 
 //问题：如何结合chmod使用
+var chmod = require('gulp-chmod');
+// .pipe(chmod(755))
+// .pipe(gulp.dest('dist'));
 
+
+//第三方服务支持
+//它是会把指定目录的内容推送到git仓库的gh-pages分支上。
+//利用git pages静态http server的特性可快速建立网站。
+var gp_deploy = require('gulp-gh-pages');
+// 指定目录
+// var options = {}
+// gulp.task('push', function () {
+//     return gulp.src('./preview/**/*')
+//         .pipe(gp_deploy(options));
+// });
 
 /******************************* 以上是模块导入 *****************************************/
 
@@ -104,28 +174,34 @@ gulp.task('dev:server', function () {
 gulp.task('compile', function() {
   // jade和scss编译
 	gulp.src('./src/page/*.jade')
-	.pipe(jade({
-	  pretty: true //pretty也就是漂亮的格式化的
-	})).pipe(gulp.dest('./dest'))
+    //给jade提供数据
+    .pipe(data(function(file,callback) {
+      // return require('./examples/' + path.basename(file.path) + '.json');
+      return require('./data/a.json');
+    }))
+  	.pipe(jade({
+  	  pretty: true //pretty也就是漂亮的格式化的
+  	}))
+    .pipe(gulp.dest('./dest'))
 
 	gulp.src('./src/css/*.scss',{base : 'src'})
-	//文件添加前缀
-	.pipe(autoprefixer({
-		browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
-        cascade: true, //是否美化属性值 默认：true 像这样：
-        //-webkit-transform: rotate(45deg);
-        //transform: rotate(45deg);
-        remove:true //是否去掉不必要的前缀 默认：true 
-	}))
-	//编译scss 
-	//style有四种编译格式:1.nested 2.expanded 3.compact 4.compressed
-	.pipe(sass({style:"nested"}))
-	// 保存一份编译完的
-	// .pipe(gulp.dest('./dest'))
-	//输出压缩文件到指定目录
-	.pipe(gulp.dest('./dest'))
-	//提醒任务完成
-	.pipe(notify({message:'====编译结束===='}))
+  	//文件添加前缀
+  	.pipe(autoprefixer({
+  		browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
+          cascade: true, //是否美化属性值 默认：true 像这样：
+          //-webkit-transform: rotate(45deg);
+          //transform: rotate(45deg);
+          remove:true //是否去掉不必要的前缀 默认：true 
+  	}))
+  	//编译scss 
+  	//style有四种编译格式:1.nested 2.expanded 3.compact 4.compressed
+  	.pipe(sass({style:"nested"}))
+  	// 保存一份编译完的
+  	// .pipe(gulp.dest('./dest'))
+  	//输出压缩文件到指定目录
+  	.pipe(gulp.dest('./dest'))
+  	//提醒任务完成
+  	.pipe(notify({message:'====编译结束===='}))
 });
 
 
@@ -147,7 +223,9 @@ gulp.task('push', function(){
 		.pipe(minifycss())
 		//给文件添加.min后缀
 		.pipe(rename({ suffix: '.min' }))
+    .pipe(chmod(755))
 		.pipe(gulp.dest('./dest/css'))
+    .pipe(size()) //我怎么没看到效果
 
 	//压缩图片
 	// gulp.src('./src/img/*.{png,jpg,gif,ico,jpeg}')
@@ -181,18 +259,25 @@ gulp.task('push', function(){
         .pipe(gulp.dest('./dest/img'))
         .pipe(notify({message:'====修改过的图片压缩结束===='}));
 
-	//压缩js
-	gulp.src('./src/*.js')
-      //只把修改过的已js结尾的文件进行编译
-      .pipe(changed('./dest/*.{js}'))
-		  .pipe(uglify({
-	        mangle: true,//类型：Boolean 默认：true 是否修改变量名
-	        compress: true,//排除混淆关键字,//类型：Boolean 默认：true 是否完全压缩
-	        preserveComments: 'all' //保留所有注释
-	    }))
-	    .pipe(rename({ suffix: '.min' }))
-	    .pipe(gulp.dest('./dest'))
-	    .pipe(notify({message:'====压缩结束===='}))
+  	//压缩js
+  	gulp.src('./src/*.js')
+        //只把修改过的已js结尾的文件进行编译
+        .pipe(changed('./dest/*.{js}'))
+  		  .pipe(uglify({
+  	        mangle: true,//类型：Boolean 默认：true 是否修改变量名
+  	        compress: true,//排除混淆关键字,//类型：Boolean 默认：true 是否完全压缩
+  	        preserveComments: 'all' //保留所有注释
+  	    }))
+  	    .pipe(rename({ suffix: '.min' }))
+  	    .pipe(gulp.dest('./dest'))
+  	    .pipe(notify({message:'====压缩结束===='}))
+
+    //压缩html
+    gulp.src('./dest/*.html')
+        //删除空格进行压缩
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dest'))
+
 })
 
 
@@ -220,3 +305,10 @@ gulp.task('del', function(){
 gulp.task('build',['default','dev:server','compile','push','watch'],function(){
 	console.log('结束任务');
 })
+
+//规定任务执行顺序
+gulp.task('sequence', gulpSequence(['a', 'b'], 'compile','push'));
+
+
+
+
